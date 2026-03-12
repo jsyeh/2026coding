bb5050786b8aaecc57d68897ede081488d688966
Jeng-Sheng Yeh 葉正聖 <jsyeh.org@gmail.com>
2026-03-12 17:31:28 +0800

    week03

--- week03/week03-3b.py
+++ week03/week03-3b.py
@@ -4,9 +4,9 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         vowels = set('aeiou')  # 把5個字母,變成set()
-        count = 0  # 記錄目前有幾個母音
+        total = 0  # 記錄目前有幾個母音
         for i in range(k):  # 找前面 k 個字母, 逐一檢查,看是不是母音
-            if s[i] in vowels: count += 1  # 找到1個母音,開心!!!
+            if s[i] in vowels: total += 1  # 找到1個母音,開心!!!
 
 
         ans = total  # 離開迴圈時, 確認前k個字母, 有 count 個母音, 先當答
